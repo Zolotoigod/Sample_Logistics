dd48387feabc851fc3f37081d643a20a011e463e
Vlad <vladshalkevich@gmail.com>
2023-01-11 16:29:46 +0100

    fix

--- MySqlRepositories/RepositoryMessages.cs
+++ MySqlRepositories/RepositoryMessages.cs
@@ -4,17 +4,17 @@ namespace MySqlReposytories
 {
     internal struct RepositoryMessages
     {
-        public const string CreateArticl = "$Create new article #{0}";
-        public const string ReadArticl = "$Read article #{0}";
+        public const string CreateArticl = "Create new article #{0}";
+        public const string ReadArticl = "Read article #{0}";
         public const string ReadCollectionArticl = "$Read collection article";
-        public const string UpdateArticl = "$Update article #{0}";
-        public const string DeleteArticl = "$Delete article #{0}";
-        public const string AddCollectionArticl = "$Add collection article";
+        public const string UpdateArticl = "Update article #{0}";
+        public const string DeleteArticl = "Delete article #{0}";
+        public const string AddCollectionArticl = $"Add collection article";
 
-        public const string CreateDocument = "$Create new document #{0}";
-        public const string ReadDocument = "$Read document #{0}";
-        public const string ReadCollectionDocument = "$Read collection document";
-        public const string UpdateDocument = "$Update document #{0}";
-        public const string DeleteDocument = "$Delete document #{0}";
+        public const string CreateDocument = "Create new document #{0}";
+        public const string ReadDocument = "Read document #{0}";
+        public const string ReadCollectionDocument = "Read collection document";
+        public const string UpdateDocument = "Update document #{0}";
+        public const string DeleteDocument = "Delete document #{0}";
     }
 }
